1b3f28b4cf6c5ac527675b815c345c973dcdcd2f
tanveerali13 <tanveer.nsu13@gmail.com>
2023-06-06 19:42:28 -0400

    update

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/home.css">
+    <link rel="stylesheet" href="assets/css/index.css">
     <title>Document</title>
 </head>
 <body>
@@ -13,7 +13,9 @@
             <a href="index.php" class="home-btn">HOME</a>
             <a href="about.php" class="about-btn">ABOUT</a>
         </div>
-        <div class="banner">
+        <div class="banner"></div>
+        <div>
+            <h1>This is a project for Web Development Environment 1</h1>
         </div>
     </section>
 </body>
