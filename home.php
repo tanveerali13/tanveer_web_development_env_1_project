5f99d0893d0e4af52871b5eb7a7fceca18cdcc62
tanveerali13 <tanveer.nsu13@gmail.com>
2023-06-06 18:23:59 -0400

    Adding initial files

---
+++ home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/home.css">
+    <title>Document</title>
+</head>
+<body>
+    <section class="homepage">
+        <div class="button">
+            <a href="index.php" class="home-btn">HOME</a>
+            <a href="about.php" class="about-btn">ABOUT</a>
+            <a href="contact.php" class="home-btn">CONTACT US</a>
+        </div>
+        <div class="banner">
+        </div>
+    </section>
+</body>
+</html>
