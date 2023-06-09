371a2f75f9134bce66a0becca0dd8f19bbf0acb0
tanveerali13 <tanveer.nsu13@gmail.com>
2023-06-08 21:27:20 -0400

    Final Update

---
+++ about.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/about.css">
+    <title>Document</title>
+</head>
+<body>
+    <section class="about">
+        <div class="button">
+            <a href="index.html" class="home-btn">HOME</a>
+            <a href="about.html" class="home-btn">ABOUT</a>
+
+        </div>
+        <div class="banner"></div>
+        <div>
+            <h1>My name is Tanveer and I am a student at Vanier College.</h1>
+            <h1>This is the final project for Web Dev Env 1</h1>
+        </div>
+    </section>
+</body>
+</html>
